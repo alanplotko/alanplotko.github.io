58ac7dabddfe4c080925a2f8fd0d87f37d1eaef1
Alan Plotko <5394528+alanplotko@users.noreply.github.com>
2021-12-26 21:10:29 -0500

    Move style to local CSS file; and optimize caching
    - Skeleton CSS framework and custom overrides separated out in dev
    - Create final stylesheet on build
    - Add production env variable for prod builds
    - Build Google Analytics on prod build only
    - Always register service worker to ensure updates
    - Simplify CSS for headers and icons
    - Update sizes where applicable to rem for consistency
    - Use visibility attribute for contact toggle to avoid content shift
    - Update caching strategy to use skipWaiting and clientsClaim
    - Add css to workbox glob patterns

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const jsonmin = require('gulp-jsonmin');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const del = require('del');
 const workboxBuild = require('workbox-build');
 
 function serviceWorker() {
     return workboxBuild.generateSW({
+        skipWaiting: true,
+        clientsClaim: true,
         globDirectory: '_site/',
         globPatterns: [
-            '**/*.{png,ico,json,pdf,html,js,svg}'
+            '**/*.{png,ico,json,pdf,html,css,js,svg}'
         ],
         swDest: '_site/sw.js',
-        ignoreURLParametersMatching: [
-            /^utm_/,
-            /^fbclid$/
-        ],
-        runtimeCaching: [
-            {
+        runtimeCaching: [{
                 urlPattern: /^https?:\/\/cdnjs.cloudflare.com/,
                 handler: 'StaleWhileRevalidate',
             },
@@ -31,7 +31,7 @@ function serviceWorker() {
     });
 }
 
-function minifyHtml() {
+function minifyHTML() {
     return gulp.src('./_site/**/*.{html,svg}')
         .pipe(htmlmin({
             collapseWhitespace: true,
@@ -45,13 +45,26 @@ function minifyHtml() {
         .pipe(gulp.dest('./_site'));
 }
 
+function minifyCSS() {
+    return gulp.src(['./_site/assets/css/skeleton.css', './_site/assets/css/custom.css'])
+        .pipe(concat('style.css'))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(gulp.dest('./_site/assets/css'));
+}
 
-function minifyJson() {
+function minifyJSON() {
     return gulp.src('./_site/**/*.json')
         .pipe(jsonmin())
         .pipe(gulp.dest('./_site'));
 }
 
-const build = gulp.series(gulp.parallel(minifyHtml, minifyJson), serviceWorker);
+function cleanUp() {
+    // Clean up source CSS files
+    return del(['./_site/**/*.css', '!./_site/assets/css/style.css']);
+}
+
+const build = gulp.series(gulp.parallel(minifyHTML, minifyCSS, minifyJSON), cleanUp, serviceWorker);
 
 exports.default = build;
