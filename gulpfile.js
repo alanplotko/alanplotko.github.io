7e4959db5e8914caa060c16a1c988effe1d23e94
Alan Plotko <5394528+alanplotko@users.noreply.github.com>
2023-01-23 18:43:37 -0500

    Refactor and add new test env config
    - Add docker folder to store docker compose files for testing external components locally
    - Refactor claps to use new nextjs app deployed via Vercel
    - Refactor comments to use giscus to start with, requiring GitHub login
    - Set up umami for event collection

--- gulpfile.js
+++ gulpfile.js
@@ -16,28 +16,28 @@ const env = process.env.JEKYLL_ENV || 'development';
  */
 
 function serviceWorker() {
-    return workboxBuild.generateSW({
-        skipWaiting: true,
-        clientsClaim: true,
-        globDirectory: '_site/',
-        globPatterns: [
-            '**/*.{png,ico,json,pdf,html,css,js,svg}'
-        ],
-        swDest: '_site/sw.js',
-        runtimeCaching: [{
-            urlPattern: /^https?:\/\/cdnjs.cloudflare.com/,
-            handler: 'StaleWhileRevalidate',
-        },
-        {
-            urlPattern: /^https?:\/\/fonts.googleapis.com/,
-            handler: 'StaleWhileRevalidate',
-        },
-        {
-            urlPattern: /^https?:\/\/fonts.gstatic.com/,
-            handler: 'StaleWhileRevalidate',
-        }
-        ]
-    });
+  return workboxBuild.generateSW({
+    skipWaiting: true,
+    clientsClaim: true,
+    globDirectory: '_site/',
+    globPatterns: [
+      '**/*.{png,ico,json,pdf,html,css,js,svg}'
+    ],
+    swDest: '_site/sw.js',
+    runtimeCaching: [{
+      urlPattern: /^https?:\/\/cdnjs.cloudflare.com/,
+      handler: 'StaleWhileRevalidate',
+    },
+    {
+      urlPattern: /^https?:\/\/fonts.googleapis.com/,
+      handler: 'StaleWhileRevalidate',
+    },
+    {
+      urlPattern: /^https?:\/\/fonts.gstatic.com/,
+      handler: 'StaleWhileRevalidate',
+    }
+    ]
+  });
 }
 
 /**
@@ -45,70 +45,70 @@ function serviceWorker() {
  */
 
 function buildHtml() {
-    return gulp.src('./_site/**/*.{html,svg}')
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            collapseInlineTagWhitespace: true,
-            conservativeCollapse: true,
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true,
-            html5: true,
-            processScripts: ['text/javascript'],
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            useShortDoctype: true
-        }))
-        .pipe(gulp.dest('./_site'));
+  return gulp.src('./_site/**/*.{html,svg}')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      collapseInlineTagWhitespace: true,
+      conservativeCollapse: true,
+      removeComments: true,
+      minifyJS: true,
+      minifyCSS: true,
+      html5: true,
+      processScripts: ['text/javascript'],
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      useShortDoctype: true
+    }))
+    .pipe(gulp.dest('./_site'));
 }
 
 function buildSass() {
-    const mainStyles = gulp.src('./assets/css/main.scss')
-        .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(purgecss({
-            content: ['./_site/index.html']
-        }))
-        .pipe(gulp.dest('./_site/assets/css', { overwrite: true }));
-    const blogStyles = gulp.src('./assets/css/theme.scss')
-        .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(purgecss({
-            content: ['./_site/**/*.html', '!./_site/index.html']
-        }))
-        .pipe(gulp.dest('./_site/assets/css', { overwrite: true }));
-    return mergeStream(mainStyles, blogStyles);
+  const mainStyles = gulp.src('./assets/css/main.scss')
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['./_site/index.html']
+    }))
+    .pipe(gulp.dest('./_site/assets/css', { overwrite: true }));
+  const blogStyles = gulp.src('./assets/css/theme.scss')
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['./_site/**/*.html', '!./_site/index.html']
+    }))
+    .pipe(gulp.dest('./_site/assets/css', { overwrite: true }));
+  return mergeStream(mainStyles, blogStyles);
 }
 
 function buildJs() {
-    return gulp.src('./assets/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./_site/assets/js', { overwrite: true }));
+  return gulp.src('./assets/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./_site/assets/js', { overwrite: true }));
 }
 
 function buildJson() {
-    return gulp.src('./_site/**/*.json')
-        .pipe(jsonmin())
-        .pipe(gulp.dest('./_site'));
+  return gulp.src('./_site/**/*.json')
+    .pipe(jsonmin())
+    .pipe(gulp.dest('./_site'));
 }
 
 function buildYaml(cb) {
-    let jekyllFiles = ['_config/common.yml'];
-    if (env === 'development') {
-        jekyllFiles.push('_config/staging.yml');
-    }
-    jekyllFiles.push(`_config/${env}.yml`);
-    cp.exec(`./node_modules/.bin/yaml-merge ${jekyllFiles.join(' ')} > _config-${env}.yml`, function (err, stdout, stderr) {
-        if (stdout) console.log(stdout);
-        if (stderr) console.log(stderr);
-        cb(err);
-    });
+  let jekyllFiles = ['_config/common.yml'];
+  if (env === 'development') {
+    jekyllFiles.push('_config/staging.yml');
+  }
+  jekyllFiles.push(`_config/${env}.yml`);
+  cp.exec(`./node_modules/.bin/yaml-merge ${jekyllFiles.join(' ')} > _config-${env}.yml`, function (err, stdout, stderr) {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    cb(err);
+  });
 }
 
 function buildStaticmanConfig(cb) {
-    cp.exec(`./node_modules/.bin/yaml-merge _config/staticman-common.yml _config/staticman-${env}.yml > staticman.yml`, function (err, stdout, stderr) {
-        if (stdout) console.log(stdout);
-        if (stderr) console.log(stderr);
-        cb(err);
-    });
+  cp.exec(`./node_modules/.bin/yaml-merge _config/staticman-common.yml _config/staticman-${env}.yml > staticman.yml`, function (err, stdout, stderr) {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    cb(err);
+  });
 }
 
 /**
@@ -116,50 +116,50 @@ function buildStaticmanConfig(cb) {
  */
 
 function watchHtml() {
-    let series = env == 'staging' ? gulp.series(buildJekyll, buildHtml, buildSass, serviceWorker) : gulp.series(buildJekyll, buildHtml, buildSass);
-    gulp.watch(['./*.html', './_includes/*.html', './_layouts/*.html'], series);
+  let series = env == 'staging' ? gulp.series(buildJekyll, buildHtml, buildSass, serviceWorker) : gulp.series(buildJekyll, buildHtml, buildSass);
+  gulp.watch(['./*.html', './_includes/*.html', './_layouts/*.html'], series);
 }
 
 function watchSass() {
-    let series = env == 'staging' ? gulp.series(buildJekyll, buildHtml, buildSass, serviceWorker) : gulp.series(buildJekyll, buildHtml, buildSass);
-    gulp.watch('./assets/css/**/*.{css,scss}', series);
+  let series = env == 'staging' ? gulp.series(buildJekyll, buildHtml, buildSass, serviceWorker) : gulp.series(buildJekyll, buildHtml, buildSass);
+  gulp.watch('./assets/css/**/*.{css,scss}', series);
 }
 
 function watchJs() {
-    let series = env == 'staging' ? gulp.series(buildJekyll, buildJs, serviceWorker) : gulp.series(buildJekyll, buildJs);
-    gulp.watch('./assets/js/*.js', series);
+  let series = env == 'staging' ? gulp.series(buildJekyll, buildJs, serviceWorker) : gulp.series(buildJekyll, buildJs);
+  gulp.watch('./assets/js/*.js', series);
 }
 
 function watchJson() {
-    let series = env == 'staging' ? gulp.series(buildJekyll, buildJson, serviceWorker) : gulp.series(buildJekyll, buildJson);
-    gulp.watch('./assets/json/*.json', series);
+  let series = env == 'staging' ? gulp.series(buildJekyll, buildJson, serviceWorker) : gulp.series(buildJekyll, buildJson);
+  gulp.watch('./assets/json/*.json', series);
 }
 
 function watchYaml() {
-    let series = env == 'staging' ? gulp.series(buildYaml, buildJekyll, serviceWorker) : gulp.series(buildYaml, buildJekyll);
-    gulp.watch('./_config/*.yml', series);
+  let series = env == 'staging' ? gulp.series(buildYaml, buildJekyll, serviceWorker) : gulp.series(buildYaml, buildJekyll);
+  gulp.watch('./_config/*.yml', series);
 }
 
 function buildJekyll(cb) {
-    cp.exec(`bundle exec jekyll build --config _config-${env}.yml`, function (err, stdout, stderr) {
-        if (stdout) console.log(stdout);
-        if (stderr) console.log(stderr);
-        cb(err);
-    });
+  cp.exec(`bundle exec jekyll build --config _config-${env}.yml`, function (err, stdout, stderr) {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    cb(err);
+  });
 }
 
 function serveJekyll(cb) {
-    cp.exec(`bundle exec jekyll serve --skip-initial-build --config _config-${env}.yml`, function (err, stdout, stderr) {
-        if (stdout) console.log(stdout);
-        if (stderr) console.log(stderr);
-        cb(err);
-    });
+  cp.exec(`bundle exec jekyll serve --skip-initial-build --config _config-${env}.yml`, function (err, stdout, stderr) {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    cb(err);
+  });
 }
 
 // Build: build yaml, build Jekyll, and build assets together. For prod, additionally purge unused css and set up the service worker.
 exports.build = (env == 'production' || env == 'staging') ?
-    gulp.series(buildYaml, buildJekyll, gulp.parallel(buildHtml, buildSass, buildJs, buildJson), serviceWorker) :
-    gulp.series(buildYaml, buildJekyll, gulp.parallel(buildHtml, buildSass, buildJs, buildJson));
+  gulp.series(buildYaml, buildJekyll, gulp.parallel(buildHtml, buildSass, buildJs, buildJson), serviceWorker) :
+  gulp.series(buildYaml, buildJekyll, gulp.parallel(buildHtml, buildSass, buildJs, buildJson));
 
 // Serve: serve Jekyll only
 exports.serve = serveJekyll;
